868cfd29de3961f2324f4064d0649f1feeca723e
Qiao <598592454@qq.com>
2018-04-26 18:29:26 +0800

    Update curlController.php

--- php/curlController.php
+++ php/curlController.php
@@ -1,12 +1,5 @@
 <?php
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
-
-use App\Http\Controllers\Controller;
 
 class curlController extends Controller
 {
